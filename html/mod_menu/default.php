6848a5e2b03f6b064f43886c9413d780a564b51e
woomaa <woomaa@op.pl>
2012-05-17 16:19:15 +0300

    Moving two divs from index.php adds ability to use navbar on other module positions:
    <div class="navbar navbar-fixed-top">
      <div class="navbar-inner">

--- html/mod_menu/default.php
+++ html/mod_menu/default.php
@@ -14,75 +14,80 @@ defined('_JEXEC') or die;
 
 // Note. It is important to remove spaces between elements.
 ?>
-<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-</a> //button for collapse menu
-<div class="nav-collapse collapse"><ul class="nav <?php echo $class_sfx; ?>"<?php
-$tag = '';
-if ($params->get('tag_id') != NULL) {
-    $tag = $params->get('tag_id') . '';
-    echo ' id="' . $tag . '"';
-}
-?>>
-    <?php if ($class_sfx === 'nav-list') : ?>
-        <li></li>
-        <li class="nav-header"><?php echo $module->title; ?></li>
-    <?php endif; ?>
-    <?php
-    foreach ($list as $i => &$item) :
-        $class = '';
-        if ($item->id == $active_id) {
-            $class .= 'current ';
-        }
 
-        if ($item->type == 'alias' &&
-                in_array($item->params->get('aliasoptions'), $path)
-                || in_array($item->id, $path)) {
-            $class .= 'active ';
-        }
-        if ($item->deeper) {
-            $class .= 'deeper ';
-        }
-
-        if ($item->parent) {
-            $class .= 'parent ';
-        }
-
-        if (!empty($class)) {
-            $class = ' class="' . trim($class) . '"';
-        }
-
-        echo '<li id="item-' . $item->id . '"' . $class . '>';
-
-        // Render the menu item.
-        switch ($item->type) :
-            case 'separator':
-            case 'url':
-            case 'component':
-                require JModuleHelper::getLayoutPath('mod_menu', 'default_' . $item->type);
-                break;
-
-            default:
-                require JModuleHelper::getLayoutPath('mod_menu', 'default_url');
-                break;
-        endswitch;
-
-        // The next item is deeper.
-        if ($item->deeper) {
-            echo '<ul class="jb-nav">';
-        }
-        // The next item is shallower.
-        else if ($item->shallower) {
-            echo '</li>';
-            echo str_repeat('</ul></li>', $item->level_diff);
-        }
-        // The next item is on the same level.
-        else {
-            echo '</li>';
-        }
-    endforeach;
-    ?>
-</ul></div>
-
+//Moving this two divs from index.php adds ability to use navbar on other module positions
+<div class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+    </a> //button for collapse menu
+    <div class="nav-collapse collapse"><ul class="nav <?php echo $class_sfx; ?>"<?php
+    $tag = '';
+    if ($params->get('tag_id') != NULL) {
+        $tag = $params->get('tag_id') . '';
+        echo ' id="' . $tag . '"';
+    }
+    ?>>
+        <?php if ($class_sfx === 'nav-list') : ?>
+            <li></li>
+            <li class="nav-header"><?php echo $module->title; ?></li>
+        <?php endif; ?>
+        <?php
+        foreach ($list as $i => &$item) :
+            $class = '';
+            if ($item->id == $active_id) {
+                $class .= 'current ';
+            }
+    
+            if ($item->type == 'alias' &&
+                    in_array($item->params->get('aliasoptions'), $path)
+                    || in_array($item->id, $path)) {
+                $class .= 'active ';
+            }
+            if ($item->deeper) {
+                $class .= 'deeper ';
+            }
+    
+            if ($item->parent) {
+                $class .= 'parent ';
+            }
+    
+            if (!empty($class)) {
+                $class = ' class="' . trim($class) . '"';
+            }
+    
+            echo '<li id="item-' . $item->id . '"' . $class . '>';
+    
+            // Render the menu item.
+            switch ($item->type) :
+                case 'separator':
+                case 'url':
+                case 'component':
+                    require JModuleHelper::getLayoutPath('mod_menu', 'default_' . $item->type);
+                    break;
+    
+                default:
+                    require JModuleHelper::getLayoutPath('mod_menu', 'default_url');
+                    break;
+            endswitch;
+    
+            // The next item is deeper.
+            if ($item->deeper) {
+                echo '<ul class="jb-nav">';
+            }
+            // The next item is shallower.
+            else if ($item->shallower) {
+                echo '</li>';
+                echo str_repeat('</ul></li>', $item->level_diff);
+            }
+            // The next item is on the same level.
+            else {
+                echo '</li>';
+            }
+        endforeach;
+        ?>
+    </ul></div>
+  </div>
+</div>
